73289bccc4e68e7ef0565cc4f89edb1c19761018
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-02-22 17:18:03 +0800

    Update LeetCode_193_Valid_Phone_Numbers.sh

--- LeetCode_193_Valid_Phone_Numbers.sh
+++ LeetCode_193_Valid_Phone_Numbers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ## Valid Number: (xxx) xxx-xxxx or xxx-xxx-xxxx for all x in [0-9]
-grep -P '^(\([0-9]{3}\)\ |[0-9]{3}\-)[0-9]{3}\-[0-9]{4}$' $1
+grep -P '^(\([[:digit:]]{3}\)\ |[[:digit:]]{3}\-)[[:digit:]]{3}\-[[:digit:]]{4}$' $1

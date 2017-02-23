e49f23d410f2899afd309d8b84ec906ff8ce96c0
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-02-23 16:52:33 +0800

    Update parallel_vs_non_awk.sh

--- parallel_vs_non_awk.sh
+++ parallel_vs_non_awk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## Use: ps -A | awk '{print $4}' | grep "^awk$" | echo "`wc -l` - 1" | bc
 ## to monitor the number of running 'awk' processes.
+
 rm -f temp.txt bigfile.txt
 
 for((i=0; i<10000; ++i)); do
@@ -12,6 +13,6 @@ for((i=0; i<3000; ++i)); do
 done
 
 time cat bigfile.txt |  awk '{s+=$1} END {print s}'
-time cat bigfile.txt | parallel --pipe awk \'{s+=\$1} END {print s}\' | awk '{s+=$1} END {print s}'
+time cat bigfile.txt | parallel --block 10M --pipe awk \'{s+=\$1} END {print s}\' | awk '{s+=$1} END {print s}'
 
 rm -f temp.txt bigfile.txt

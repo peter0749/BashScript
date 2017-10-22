7498fc8d0671ba8b92b42e869def8b4189f70c80
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-10-22 10:17:48 +0800

    Update batch_midi_hist.sh

--- batch_midi_hist.sh
+++ batch_midi_hist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find data -type f -iname '*.mid' -exec sh -c 'fm=$(echo "{}" | cut -d"/" -f2);  ./make_histogram.sh "{}" > "hist/$fm.hist"' \;
+find data -type f -iname '*.mid' -exec sh -c 'fm=$(echo "{}" | cut -d"/" -f2) && ./make_histogram.sh "{}" > "hist/$fm.hist"' \;

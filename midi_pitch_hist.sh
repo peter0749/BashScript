b4e1c58a01fe6cb3a48d772d9f5280feb2e51864
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-10-22 09:49:26 +0800

    Update midi_pitch_hist.sh

--- midi_pitch_hist.sh
+++ midi_pitch_hist.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-grep Note_on_c test.csv | tr -d ' ' | cut -d ',' -f 5 | awk ' BEGIN{ FS=" " } { for(i=1; i<=NF; ++i) S[$i]++; } END{ for(v in S){ print v " " S[v] } } ' | sort -n -k1
+
+csvfile=$(mktemp)
+
+midicsv "$1" "$csvfile"
+
+grep Note_on_c "$csvfile" | tr -d ' ' | cut -d ',' -f 5 | awk ' BEGIN{ FS=" " } { for(i=1; i<=NF; ++i) S[$i]++; } END{ for(v in S){ print v " " S[v] } } ' | sort -n -k1
+
+rm "$csvfile"

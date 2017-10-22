71f9a021229e4460c3d79557951c925ed140ffc7
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-10-22 12:03:56 +0800

    Create delete_same_track.sh

---
+++ delete_same_track.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+import sys
+import os
+import midi
+
+fn = str(sys.argv[1])
+patt = midi.read_midifile(fn)
+
+if len(patt[0])!=len(patt[1]):
+    exit(0)
+
+for i in xrange(len(patt[0])):
+    a = patt[0][i]
+    b = patt[1][i]
+    if a!=b:
+        if isinstance(a, midi.NoteOnEvent) and type(a)==type(b):
+            if a.tick!=b.tick or a.data[0]!=b.data[0]:
+                exit(0)
+
+os.unlink(fn)
+exit(1)

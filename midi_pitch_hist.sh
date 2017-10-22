ca8c0828b91710c4287efd2487e4c579fd720d10
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-10-22 10:05:58 +0800

    Update midi_pitch_hist.sh

--- midi_pitch_hist.sh
+++ midi_pitch_hist.sh
@@ -4,13 +4,14 @@ csvfile=$(mktemp)
 
 midicsv "$1" "$csvfile"
 
-colCnt=`grep Note_on_c "$csvfile" | awk '{FS=","} {print NF}' | sort -nu | head -n1`
+colCnt=`grep Note_on_c "$csvfile" | awk 'BEGIN{FS=","} {print NF}' | sort -nu | head -n1`
 
 if [[ $colCnt -lt 5 ]]; then
+    rm "$csvfile"
     exit 1 ## error
 fi
 
-grep Note_on_c "$csvfile" | tr -d ' ' | cut -d ',' -f 5 | awk ' BEGIN{ FS=" " } { for(i=1; i<=NF; ++i) S[$i]++; } END{ for(v in S){ print v " " S[v] } } ' | sort -n -k1
+grep Note_on_c "$csvfile" | tr -d ' ' | awk ' BEGIN{ FS="," } { S[$5]++; } END{ for(v in S){ print v " " S[v] } } ' | sort -n -k1
 
 rm "$csvfile"
 

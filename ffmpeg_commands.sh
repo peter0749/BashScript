05b82d96ff3d50beb3fab20c31b996948d19e44a
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-12-05 20:44:27 +0800

    Update ffmpeg_commands.sh

--- ffmpeg_commands.sh
+++ ffmpeg_commands.sh
@@ -1,3 +1,4 @@
 ffmpeg -i 【PS4FT】Luka\ Luka★Night\ Fever【Megurine\ Luka：V4X】-BkHJ495HOKI.mp4 -filter:v "fps=30, scale=454:256, crop=256:256:99:0"   -qscale:v 2 -huffman optimal luka_%08d.jpg
 ffmpeg -i haku_00005354.jpg -i 00005287.jpg -filter_complex hstack output.jpg
 nohup ls haku | sort -n | parallel -u --block 100M 'python inference.py --model "haku2luka/haku2luka.pb" --input "haku/{}" --output "./result/{}"' &
+ffmpeg -framerate 30 -i haku/%08d.jpg -framerate 30 -i result/%08d.jpg -filter_complex hstack -f mp4 -q:v 0 output.mp4

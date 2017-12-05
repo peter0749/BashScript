ec81329a05e7839160f7bd4665de9505650ff761
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-12-05 20:34:22 +0800

    Update ffmpeg_commands.sh

--- ffmpeg_commands.sh
+++ ffmpeg_commands.sh
@@ -1,1 +1,2 @@
 ffmpeg -i 【PS4FT】Luka\ Luka★Night\ Fever【Megurine\ Luka：V4X】-BkHJ495HOKI.mp4 -filter:v "fps=30, scale=454:256, crop=256:256:99:0"   -qscale:v 2 -huffman optimal luka_%08d.jpg
+ffmpeg -i haku_00005354.jpg -i 00005287.jpg -filter_complex hstack output.jpg
